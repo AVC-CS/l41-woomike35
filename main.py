8a64f1f905ba0e4f7c26c545c528d8b7ab25af7c
woomike35 <michaeleze226@gmail.com>
2025-04-16 12:00:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 def main():
+    N = int(input("Enter the number N:10"))
 
-    N = int(input('Enter the number N: '))
-    result = []
+    result = [2 ** i for i in range(N + 1)]
 
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    print(*result)
 
-    ########################################
-    # Do not delete the return statement
-    ########################################
     return result
 
-
 if __name__ == '__main__':
-    main()
+    result = main()
